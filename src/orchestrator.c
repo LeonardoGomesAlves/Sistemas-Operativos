ae820008140ce439100f4c15219e64bf79aeba1b
BatataMic <a104614@alunos.uminho.pt>
2024-04-16 16:01:40 +0100

    fiz com que o output apareça num módulo de outputs

--- src/orchestrator.c
+++ src/orchestrator.c
@@ -47,11 +47,22 @@ int main (int argc, char* argv[]) {
                 perror("fork_filho");
                 return 1;
             } else if (filho_pid == 0) {
-                if (execvp(commands[0],commands) == -1) 
+                int stdout = dup(1);
+                char* aux = "output";
+                char* output = strdup(buf.pid_path);
+                strcat(output,aux);
+                int file_out = open(output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if(file_out == -1){
+                    perror("open");
+                    _exit(-1);
+                }
+                dup2(file_out,1);
+                if(execvp(commands[0],commands) == -1) 
                 {
                     perror("execvp");
                     return 1;
                 }
+                close(file_out);
                 _exit(0);
             }
             else {
