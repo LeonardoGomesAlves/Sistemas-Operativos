91e5a91ffa8ace4c17bbcf636a4c9f0c7bcf5010
Leonardo <leonardogomesalves1@gmail.com>
2024-05-05 23:39:03 +0100

    fix script

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for ((i = 1; i <= 10; i++))
+for ((i = 1; i <= 100; i++))
 do
     echo "./client execute 100 -u "man ls""
-    echo "./client execute 2 -p "grep -v ˆ# /etc/passwd | cut -f7 -d: | uniq | wc -l.""
-    echo "./client execute 1000 - u "sleep 1""
+    echo "./client execute 2 -p "grep -v ˆ# /etc/passwd | cut -f7 -d: | uniq | wc -l""
+    echo "./client execute 1000 -u "sleep 1""
 
     ./client execute 100 -u "man ls"
-    ./client execute 2 -p "grep -v ˆ# /etc/passwd | cut -f7 -d: | uniq | wc -l."
-    ./client execute 1000 - u "sleep 1"
+    ./client execute 2 -p "grep -v ˆ# /etc/passwd | cut -f7 -d: | uniq | wc -l"
+    ./client execute 1000 -u "sleep 1"
 done
